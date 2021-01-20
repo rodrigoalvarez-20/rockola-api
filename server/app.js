6dce552c01153b3ca069ce2b2f338712dc60860d
rodrigoalvarez-20 <rodrigoalvarez449@gmail.com>
2021-01-20 10:29:57 -0600

    Cors

--- server/app.js
+++ server/app.js
@@ -21,10 +21,10 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-//app.use(cors());
+app.use(cors());
 app.use(morgan("dev"));
 
-app.use(function (req, res, next) {
+/* app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
@@ -33,7 +33,7 @@ app.use(function (req, res, next) {
     "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
   );
   next();
-});
+}); */
 
 /* app.options("*", cors());
 
