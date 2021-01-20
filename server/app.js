e0250a1c70ce05a3b0ec5779643194e3f6855e80
rodrigoalvarez-20 <rodrigoalvarez449@gmail.com>
2021-01-20 03:04:58 -0600

    Maybe Cors

--- server/app.js
+++ server/app.js
@@ -24,6 +24,21 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan("dev"));
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  next();
+});
+
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Pagina principal de la api" });
 });
